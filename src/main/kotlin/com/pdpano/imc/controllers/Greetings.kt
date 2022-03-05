a462000524a3cd47a9a2908bd00207e92fcf8c2d
pdpan0 <lm.m4rtins@gmail.com>
2022-03-04 22:56:35 -0300

    FEAT (profiles): Adicionando perfis para cada ambiente da aplicação

--- src/main/kotlin/com/pdpano/imc/controllers/Greetings.kt
+++ src/main/kotlin/com/pdpano/imc/controllers/Greetings.kt
@@ -1,14 +1,17 @@
 package com.pdpano.imc.controllers
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class Greetings {
+class Greetings(
+    @Value("\${pdpano.imc.buildNumber}") private val buildNumber: String
+) {
     @GetMapping("/greetings")
-    fun greetings() = ResponseEntity.ok("Olá amigo.")
+    fun greetings() = ResponseEntity.ok(buildNumber)
 
     @PostMapping("/login")
     fun login() = ResponseEntity.ok("token")
